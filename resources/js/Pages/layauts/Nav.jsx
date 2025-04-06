c15cba05d1925be0a3f6fc85754b806da2bf952e
romel <wiracocha.covid19@gmail.com>
2025-04-06 02:32:54 -0400

    agregando vistas

--- resources/js/Pages/layauts/Nav.jsx
+++ resources/js/Pages/layauts/Nav.jsx
@@ -64,7 +64,7 @@ const Nav = () => {
                 <NavigationMenuLink asChild>
                   <a
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md mb-0"
-                    href="/"
+                    href="#seccion_nosotros"
                   >
                     <img src={logo} alt="Logo de Veterinaria FIRU" className="w-16 h-16 mb-4" />
                     <div className="mb-2 mt-4 text-lg font-medium">
