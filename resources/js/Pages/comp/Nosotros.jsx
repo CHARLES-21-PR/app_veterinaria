853f439d2243d2c6b10a10ce4e11db1b697ff71f
romel <wiracocha.covid19@gmail.com>
2025-04-05 23:38:36 -0400

    agregando algo

---
+++ resources/js/Pages/comp/Nosotros.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const nosotros = () => {
+  return (
+    <div className='nosotros'>
+        <h1>Nosotros</h1>
+        <h3>Queremos mejorar la calidad de vida de cada mascota.</h3>
+        <p>Promovemos la excelencia en la atención veterinaria a través del uso de prácticas clínicas avanzadas, tecnología de última generación e innovación constante.</p>
+        <p>Impulsamos la prevención como clave para evitar enfermedades, garantizando una vida larga y saludable para cada mascota. </p>
+        <p>Y tratamos las enfermedades con un enfoque efectivo y personalizado, utilizando los mejores recursos para asegurar su recuperación y bienestar.</p>
+        <p>Ofrecemos una atención veterinaria integral y de alta calidad, respaldada por 5 pilares fundamentales: ética profesional, excelencia médica, tecnología avanzada, prácticas basadas en evidencia científica y un trato amable y respetuoso.</p>
+        
+    </div>
+    
+  )
+}
+
+export default nosotros
