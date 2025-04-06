a877e6baaf8c72e1be2b8695588a611c7c17abd2
CHARLES-21-PR <2301010144@undc.edu.pe>
2025-04-05 22:35:12 -0500

    [LIGTH] first version

--- resources/js/Pages/layauts/Flutter.jsx
+++ resources/js/Pages/layauts/Flutter.jsx
@@ -3,8 +3,18 @@ import React from 'react'
 const Flutter = () => {
   return (
     <div className='flutter'>
-        <div className='name_vete'>
+        <div className='flutter_vete'>
             <h3 className='mb-0'>veterinaria<span> FIRU</span></h3>
+            <p>Av. Aviación 3001, San Borja, Lima - Perú.</p>
+            <div className='flutter_details'>
+                <div className="contactos_flutter">
+                    <h3>Contacto</h3>
+                    <p>Central Telefónica: 01 748 8000</p>
+                </div>
+                <div className="redes_flutter">
+                    <h3>Redes Sociales</h3>
+                </div>
+            </div>
         </div>
     </div>
   )
